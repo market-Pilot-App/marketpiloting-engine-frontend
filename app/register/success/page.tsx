3b153cf1b1c8625d3ff1d76be43f8edc7041f1c7
AltarCloud <altercloud007@gmail.com>
2026-07-28 04:13:09 +0100

    fix: correct useSearchParams import on success page

--- app/register/success/page.tsx
+++ app/register/success/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useSearchParams, Suspense } from "react";
+import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 
 function SuccessContent() {
